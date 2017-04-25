ed630534003827b0dc2ec3fbcb6ba7750512292a
litwisha <litwisha@gmail.com>
2017-04-25 13:59:00 +0300

    test clear_scroll method and has_more property

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -2,6 +2,7 @@ import asyncio
 
 import pytest
 
+from aioelasticsearch import NotFoundError
 from aioelasticsearch.helpers import Scan
 
 from tests.utils import populate
@@ -58,8 +59,8 @@ def test_scan_equal_chunks(loop, es, scroll_size):
             for doc in docs:
                 ids.add(doc['_id'])
 
-    # check number of unique doc ids
-    assert len(ids) == n
+        # check number of unique doc ids
+        assert len(ids) == n == scan.total
 
     # check number of docs in a scroll
     expected_scroll_sizes = [scroll_size] * (n // scroll_size)
@@ -68,3 +69,58 @@ def test_scan_equal_chunks(loop, es, scroll_size):
 
     scroll_sizes = [len(scroll) for scroll in data]
     assert scroll_sizes == expected_scroll_sizes
+
+
+@pytest.mark.run_loop
+@asyncio.coroutine
+def test_scan_has_more(loop, es):
+    index = 'test_aioes'
+    doc_type = 'type_1'
+    n = 10
+    scroll_size = 3
+    yield from populate(es, index, doc_type, n, loop=loop)
+
+    with Scan(
+        es,
+        index=index,
+        doc_type=doc_type,
+        size=scroll_size,
+    ) as scan:
+        yield from scan.scroll()
+        assert scan.has_more
+
+        for scroll in scan:
+            yield from scroll
+
+        with pytest.raises(StopIteration):
+            next(scan)
+
+        assert not scan.has_more
+
+
+@pytest.mark.run_loop
+@asyncio.coroutine
+def test_scan_clear_scroll(loop, es):
+    index = 'test_aioes'
+    doc_type = 'type_1'
+    n = 10
+    scroll_size = 3
+    yield from populate(es, index, doc_type, n, loop=loop)
+
+    with Scan(
+        es,
+        index=index,
+        doc_type=doc_type,
+        size=scroll_size,
+    ) as scan:
+        yield from scan.scroll()
+
+        yield from scan.clear_scroll()
+
+        with pytest.raises(NotFoundError):
+            for scroll in scan:
+                yield from scroll
+
+        # run cleared scroll
+        with pytest.raises(AssertionError):
+            yield from scan.scroll()
