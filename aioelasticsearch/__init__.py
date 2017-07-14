e1c21195a3e93ebf9a6bb86b12d7f4450b639549
hellysmile <hellysmile@gmail.com>
2017-07-14 15:56:42 +0300

    Bump version

--- aioelasticsearch/__init__.py
+++ aioelasticsearch/__init__.py
@@ -12,7 +12,7 @@ from .pool import AIOHttpConnectionPool  # noqa # isort:skip
 from .transport import AIOHttpTransport  # noqa # isort:skip
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 class Elasticsearch(_Elasticsearch):
