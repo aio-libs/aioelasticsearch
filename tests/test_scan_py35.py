f308a7940eb5d4491f616fa59abc2edd7eddb6f5
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-08-01 16:05:52 +0300

    isort imports

--- tests/test_scan_py35.py
+++ tests/test_scan_py35.py
@@ -1,4 +1,5 @@
 import pytest
+
 from aioelasticsearch.helpers import Scan
 
 
--- tests/test_transport.py
+++ tests/test_transport.py
@@ -1,4 +1,5 @@
 import pytest
+
 from aioelasticsearch import AIOHttpTransport
 from aioelasticsearch.connection import AIOHttpConnection
 
