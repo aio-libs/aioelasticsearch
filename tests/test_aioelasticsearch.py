b430afc9fe04190dc9039e97c3a37cd4544127c4
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-08-02 16:16:34 +0300

    Add new test

--- tests/test_aioelasticsearch.py
+++ tests/test_aioelasticsearch.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 
 from aioelasticsearch import Elasticsearch
@@ -16,3 +18,13 @@ async def test_str_auth(es, es_server, loop):
                                                 'port': es_server['port']}],
                              http_auth=':'.join(es_server['auth'])) as es1:
         await es1.ping()
+
+
+@pytest.mark.run_loop
+async def test_elastic_default_loop(es, loop, es_server):
+    asyncio.set_event_loop(loop)
+
+    async with Elasticsearch(hosts=[{'host': es_server['host'],
+                                     'port': es_server['port']}],
+                             http_auth=':'.join(es_server['auth'])) as es1:
+        assert es1._loop is loop
