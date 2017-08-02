1342de10d29b8a0b017391243e64b28c301e2fa5
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-08-02 11:33:09 +0300

    Update required aiohttp version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     long_description=read('README.rst'),
     install_requires=[
         'elasticsearch>=5.4.0,<6.0.0',
-        'aiohttp>=1.3.0',
+        'aiohttp>=2.0.0',
     ],
     python_requires='>=3.5.0',
     packages=['aioelasticsearch'],
