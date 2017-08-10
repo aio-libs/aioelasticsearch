fbe53bf09c758629a9226728d9f6b82b97655be1
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-08-10 16:19:23 +0300

    Add tests for connection

---
+++ tests/test_connection.py
@@ -0,0 +1,52 @@
+import aiohttp
+import pytest
+
+
+from aioelasticsearch.connection import AIOHttpConnection
+
+
+@pytest.mark.run_loop
+async def test_default_headers(auto_close, loop):
+    conn = auto_close(AIOHttpConnection(loop=loop))
+    assert conn.headers == {'Content-Type': 'application/json'}
+
+
+@pytest.mark.run_loop
+async def test_custom_headers(auto_close, loop):
+    conn = auto_close(AIOHttpConnection(headers={'X-Custom': 'value'},
+                                        loop=loop))
+    assert conn.headers == {'Content-Type': 'application/json',
+                            'X-Custom': 'value'}
+
+
+@pytest.mark.run_loop
+async def test_auth_no_auth(auto_close, loop):
+    conn = auto_close(AIOHttpConnection(loop=loop))
+    assert conn.http_auth is None
+
+
+@pytest.mark.run_loop
+async def test_auth_str(auto_close, loop):
+    auth = aiohttp.BasicAuth('user', 'pass')
+    conn = auto_close(AIOHttpConnection(http_auth='user:pass', loop=loop))
+    assert conn.http_auth == auth
+
+
+@pytest.mark.run_loop
+async def test_auth_tuple(auto_close, loop):
+    auth = aiohttp.BasicAuth('user', 'pass')
+    conn = auto_close(AIOHttpConnection(http_auth=('user', 'pass'), loop=loop))
+    assert conn.http_auth == auth
+
+
+@pytest.mark.run_loop
+async def test_auth_basicauth(auto_close, loop):
+    auth = aiohttp.BasicAuth('user', 'pass')
+    conn = auto_close(AIOHttpConnection(http_auth=auth, loop=loop))
+    assert conn.http_auth == auth
+
+
+@pytest.mark.run_loop
+async def test_auth_invalid(loop):
+    with pytest.raises(TypeError):
+        AIOHttpConnection(http_auth=object(), loop=loop)
