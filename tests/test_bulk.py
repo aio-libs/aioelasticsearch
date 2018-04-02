a7374072be983268b99568433c0a8cb975a187b5
RainJay <rainjay0117@gmail.com>
2018-04-02 09:21:42 +0800

    fix assert error

--- tests/test_bulk.py
+++ tests/test_bulk.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from unittest import mock
-
 import pytest
 
-from aioelasticsearch import NotFoundError
 from aioelasticsearch.helpers import bulk
 
-
 logger = logging.getLogger('elasticsearch')
+
 
 def gen_data1():
     for i in range(10):
@@ -18,8 +15,9 @@ def gen_data1():
                "_id": str(i),
                "foo": "1"}
 
+
 def gen_data2():
-    for i in range(10,20):
+    for i in range(10, 20):
         yield {"_index": "test_aioes",
                "_type": "type_3",
                "_id": str(i),
@@ -32,15 +30,15 @@ async def test_bulk_simple(es):
     success, fails = await bulk(es, gen_data1(),
                                 concurrency_limit=2,
                                 stats_only=True)
-    await  success == 10
+    assert success == 10
     assert fails == 0
-
 
     success, fails = await bulk(es, gen_data2(),
                                 concurrency_limit=2,
                                 stats_only=True)
-    await  success == 10
+    assert success == 10
     assert fails == 0
+
 
 @pytest.mark.run_loop
 async def test_bulk_fails(es):
@@ -48,6 +46,6 @@ async def test_bulk_fails(es):
               '_index': 'test_aioes',
               '_type': 'type_3', '_id': "999"}
              ]
-    success, fails = await bulk(es,datas,stats_only=True)
-    await success == 0
-    await success == 1
+    success, fails = await bulk(es, datas, stats_only=True)
+    assert success == 0
+    assert success == 1
