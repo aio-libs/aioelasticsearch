4ecef47ccb2ce4d02108fea3b17c0d34266196a9
rainjay <RainJay0117@gmail.com>
2018-04-08 00:04:51 +0800

    update test

--- tests/test_bulk.py
+++ tests/test_bulk.py
@@ -29,13 +29,11 @@ def gen_data2():
 @pytest.mark.run_loop
 async def test_bulk_simple(es):
     success, fails = await bulk(es, gen_data1(),
-                                concurrency_limit=2,
                                 stats_only=True)
     assert success == 10
     assert fails == 0
 
     success, fails = await bulk(es, gen_data2(),
-                                concurrency_limit=2,
                                 stats_only=True)
     assert success == 10
     assert fails == 0
@@ -43,10 +41,11 @@ async def test_bulk_simple(es):
 
 @pytest.mark.run_loop
 async def test_bulk_fails(es):
-    datas = [{'op_type': 'delete',
+    datas = [{'_op_type': 'delete',
               '_index': 'test_aioes',
               '_type': 'type_3', '_id': "999"}
              ]
+
     success, fails = await bulk(es, datas, stats_only=True)
     assert success == 0
-    assert success == 1
+    assert fails == 1
